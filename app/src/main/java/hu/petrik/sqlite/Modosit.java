140b1bc2693e1bf57b2bfadc96404a3226be5e8b
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-22 17:43:23 +0100

    Módosítás activityben vissza gomb működik

--- app/src/main/java/hu/petrik/sqlite/Modosit.java
+++ app/src/main/java/hu/petrik/sqlite/Modosit.java
@@ -2,13 +2,40 @@ package hu.petrik.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Modosit extends AppCompatActivity {
+
+    private EditText editId, editVeznev, editJernev, editJegy;
+    private Button button_modositas, button_visszaModositas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modosit);
+
+        init();
+
+        button_visszaModositas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Modosit.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void init() {
+        editId = findViewById(R.id.editId);
+        editVeznev = findViewById(R.id.editVeznev);
+        editJernev = findViewById(R.id.editJernev);
+        editJegy = findViewById(R.id.editJegy);
+        button_modositas = findViewById(R.id.button_modositas);
+        button_visszaModositas = findViewById(R.id.button_visszaModositas);
     }
 }
